f3cd8ef2d1dafd32a287d4972ba9e0272d248ffc
lyz <lyz12918@163.com>
2016-08-11 17:45:23 +0800

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token
+	has_secure_password
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+	attr_accessor :remember_token, :activation_token
 
 	# before_save { self.email = email.downcase }
 	# other way
 	before_save { email.downcase! }
-	has_secure_password
+	before_create :create_activation_digest
 
 	validates :name, presence: true, length: { maximum: 40 }
 	validates :email, presence: true, length: { maximum: 255 },
@@ -28,12 +30,28 @@ class User < ApplicationRecord
 		update_attribute(:remember_digest, User.digest(remember_token))
 	end
 
-	def authenticated?(remember_token)
-		return false if remember_digest.nil?
-		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	def authenticated?(attribute, token)
+		digest = send("#{attribute}_digest")
+		return false if digest.nil?
+		BCrypt::Password.new(digest).is_password?(token)
 	end
 
 	def forget
 		update_attribute(:remember_digest, nil)
+	end
+
+	def activate
+		update_attribute(:activated, true)
+		update_attribute(:activated_at, Time.zone.now)
+	end
+
+	def send_activation_email
+		UserMailer.account_activation(self).deliver_now
+	end
+
+private
+	def create_activation_digest
+		self.activation_token = User.new_token
+		self.activation_digest = User.digest(activation_token)
 	end
 end
